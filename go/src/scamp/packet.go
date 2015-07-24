4afb524bf8cc4b6d0783277825067bd809247541
Xavier Lange <xrlange@gmail.com>
2015-07-23 18:35:41 -0700

    Got reflection tests passing but didn't put it in the hot path. Oops.

--- go/src/scamp/packet.go
+++ go/src/scamp/packet.go
@@ -88,8 +88,7 @@ func ReadPacket(reader io.Reader) (Packet,error) {
   }
 
   if pkt.packetType == HEADER {
-    err := pkt.ParseHeaderManual()
-    // err := pkt.ParseHeaderReflection()
+    err := pkt.ParseHeaderReflection()
     if err != nil {
       return Packet{}, err
     }
@@ -98,59 +97,10 @@ func ReadPacket(reader io.Reader) (Packet,error) {
   return pkt,nil
 }
 
-func (pkt *Packet)ParseHeaderManual() (err error) {
-  header := PacketHeader{}
-
-  var objmap map[string]*json.RawMessage
-  err = json.Unmarshal(pkt.body, &objmap)
-  if err != nil {
-    return err
-  }
-
-  var action string
-  err = json.Unmarshal(*objmap["action"], &action)  
-  if err != nil {
-    return err
-  } else {
-    header.Action = action
-  }
-
-  var version int64
-  err = json.Unmarshal(*objmap["version"], &version)  
-  if err != nil {
-    return err
-  } else {
-    header.Version = version
-  }
-
-  var envelope string
-  err = json.Unmarshal(*objmap["envelope"], &envelope)
-  if err != nil {
-    return err
-  } else {
-    switch envelope {
-    case "json":
-      header.Envelope = ENVELOPE_JSON
-    case "jsonstore":
-      header.Envelope = ENVELOPE_JSONSTORE
-    default:
-      return errors.New( fmt.Sprintf("envelope `%s` is not valid", envelope) )
-    }
-  }
-
-  pkt.packetHeader = header
-
-  return nil
-}
-
 // TODO: why does reflection-based decoding
 // fail to extract any values?
 func (pkt *Packet)ParseHeaderReflection() (err error) {
-  header := PacketHeader{}
-
-  pkt.packetHeader = header
-
-  err = json.Unmarshal(pkt.body, &header)
+  err = json.Unmarshal(pkt.body, &pkt.packetHeader)
   if err != nil {
     return
   }
