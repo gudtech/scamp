2df5081bbfa3c70377cf7b5cb499f552ad024839
Xavier Lange <xrlange@gmail.com>
2015-07-29 18:45:01 -0700

    bin folder was being skipped

---
+++ go/bin/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "scamp"
+
+func main() {
+	scamp.Initialize()
+
+	conn, err := scamp.Connect("127.0.0.1:30101")
+	defer conn.Close()
+
+	if err != nil {
+		scamp.Error.Fatalf("could not connect! `%s`\n", err)
+	}
+
+	var sess *scamp.Session
+
+	sess, err = conn.Send(scamp.Request{
+		Action:         "helloworld.hello",
+		EnvelopeFormat: scamp.ENVELOPE_JSON,
+		Version:        1,
+	})
+	if err != nil {
+		scamp.Error.Fatalf("error initiating session: `%s`", err)
+	}
+
+	reply, err := sess.RecvReply()
+	if err != nil {
+		scamp.Error.Fatalf("error receiving: `%s`", err)
+	}
+	scamp.Info.Printf("Got reply! `%s`", reply.Blob)
+}
